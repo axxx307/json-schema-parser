aabe264bc50349c87b675519b21c24de40ce5257
Mark Parfenov <mark.pfnov@gmail.com>
2021-07-29 19:16:54 +0300

    Update new.ts

--- src/new.ts
+++ src/new.ts
@@ -18,39 +18,6 @@ import {
   twoPropertyObjectNullableInvesred
 } from "./testcases";
 
-function combine(properties: Enum) {
-  return _(properties).reduce((acc, val) => {
-    if (acc.includes(val)) {
-      return acc;
-    }
-
-    if (acc.length !== 0) {
-      acc += " | ";
-    }
-
-    acc += val ? `${val}` : val;
-    return acc;
-  }, "");
-}
-
-function combineEnum(enumProperties: Enum) {
-  return _(enumProperties)
-    .filter((val) => val !== null)
-    .reduce((acc, val) => {
-      if (acc.length !== 0) {
-        acc += " | ";
-      }
-
-      if (val === "") {
-        acc += "''";
-      } else {
-        acc += `'${val}'`;
-      }
-
-      return acc;
-    }, "");
-}
-
 type PropertyType = "string" | "number" | "object" | "boolean" | "null";
 
 type Enum = (string | null)[];
@@ -81,64 +48,108 @@ type Schema = {
    6. Retrieve all types for specific key, combine them via .combine
 */
 
-function processType(
-  propertyType: PropertyType,
-  property: Property
-): string | null {
-  return match(propertyType)
-    .with("null", () => null)
-    .with("string", (vl: string) => {
-      return property.enum ? combineEnum(property.enum) : vl;
-    })
-    .with(not("object"), (vl: string) => vl)
-    .exhaustive();
-}
+function Main() {
+  function combine(properties: Enum) {
+    return _(properties).reduce((acc, val) => {
+      if (acc.includes(val)) {
+        return acc;
+      }
 
-function processProperty(fieldName: string, property: Property) {
-  const { type } = property;
+      if (acc.length !== 0) {
+        acc += " | ";
+      }
 
-  const processObjectProperties = (properties: Properties) => {
-    const nestedProperties = _(properties)
-      .map((property: Property, key: string) => processProperty(key, property))
-      .join("");
-    return `{ ${nestedProperties} }`;
+      acc += val ? `${val}` : val;
+      return acc;
+    }, "");
+  }
+
+  function combineEnum(enumProperties: Enum) {
+    return _(enumProperties)
+      .filter((val) => val !== null)
+      .reduce((acc, val) => {
+        if (acc.length !== 0) {
+          acc += " | ";
+        }
+
+        if (val === "") {
+          acc += "''";
+        } else {
+          acc += `'${val}'`;
+        }
+
+        return acc;
+      }, "");
+  }
+
+  function processType(
+    propertyType: PropertyType,
+    property: Property
+  ): string | null {
+    return match(propertyType)
+      .with("null", () => null)
+      .with("string", (vl: string) => {
+        return property.enum ? combineEnum(property.enum) : vl;
+      })
+      .with(not("object"), (vl: string) => vl)
+      .exhaustive();
+  }
+
+  function processProperty(fieldName: string, property: Property) {
+    const { type } = property;
+
+    const processObjectProperties = (
+      properties: Properties | undefined
+    ): string => {
+      const nestedProperties = _(properties)
+        .map((property: Property, key: string) =>
+          processProperty(key, property)
+        )
+        .join("");
+
+      return `{ ${nestedProperties} }`;
+    };
+
+    const fieldType: string | null = match(type)
+      .with("object", () => {
+        if (property.properties) {
+          return processObjectProperties(property.properties);
+        } else {
+          return `{ [key: string]: any; }`;
+        }
+      })
+      .with([__, __], (types: PropertyType[]) => {
+        if (types.indexOf("object") > -1) {
+          const singleTypeObject = processObjectProperties(property.properties);
+          return combine([singleTypeObject, null]);
+        }
+
+        return combine([
+          processType(types[0], property),
+          processType(types[1], property)
+        ]);
+      })
+      .with(__, (t: PropertyType) => processType(t, property))
+      .exhaustive();
+    return `${fieldName}: ${fieldType};`;
+  }
+
+  /*
+    step 1. simple generation with types and interfaces
+    step 2. support enums
+    step 3. make support for foreign keys
+  */
+
+  function process(name: string, schema: Schema) {
+    const mappedProperties: string = _(schema.properties)
+      .map((value: Property, key: string) => processProperty(key.trim(), value))
+      .join("\n");
+    return `export interface ${name} { ${mappedProperties} }`;
+  }
+
+  return {
+    process
   };
-
-  const fieldType = match(type)
-    .with("object", () => {
-      if (property.properties) {
-        return processObjectProperties(property.properties);
-      } else {
-        return `{ [key: string]: any; }`;
-      }
-    })
-    .with([__, __], (types: PropertyType[]) => {
-      if (types.indexOf("object") > -1) {
-        const singleTypeObject = processObjectProperties(property.properties);
-        return combine([singleTypeObject, null]);
-      }
-
-      return combine([
-        processType(types[0], property),
-        processType(types[1], property)
-      ]);
-    })
-    .with(__, (t: PropertyType) => processType(t, property))
-    .exhaustive();
-  return `${fieldName}: ${fieldType};`;
-}
-
-/*
-  step 1. simple generation with types and interfaces
-  step 2. support enums
-  step 3. make support for foreign keys
-*/
-
-function process(name: string, schema: Schema) {
-  const mappedProperties: string = _(schema.properties)
-    .map((value: Property, key: string) => processProperty(key.trim(), value))
-    .join("\n");
-  return `export interface ${name} { ${mappedProperties} }`;
 }
 
 const test = {
@@ -146,7 +157,7 @@ const test = {
   properties: job
 };
 
-const result = process("Test", test);
+const result = Main().process("Test", test);
 console.log(result);
 
 document.getElementById("app").innerHTML = `
